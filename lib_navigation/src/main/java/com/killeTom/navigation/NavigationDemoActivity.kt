5f22dc52e4560044025280064766d0d34da7a3b5
KilleTom <1986545332@qq.com>
2020-09-13 19:09:38 +0800

    Initial commit

---
+++ lib_navigation/src/main/java/com/killeTom/navigation/NavigationDemoActivity.kt
@@ -0,0 +1,11 @@
+package com.killeTom.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavigationDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_demo)
+    }
+}
---
+++ lib_navigation/src/main/java/com/killeTom/navigation/fragment/NavDemoFragmentC.kt
@@ -0,0 +1,19 @@
+package com.killeTom.navigation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.killeTom.navigation.R
+
+class NavDemoFragmentC :Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.nav_demo_fragment_c, container, false)
+    }
+}
